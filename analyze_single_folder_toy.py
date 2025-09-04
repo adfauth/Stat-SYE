52c7da055510acaebf6ffc5aa86e17f0731a16c6
Aidan Fauth <adfaut22@stlawu.edu>
2025-09-04 15:23:28 -0400

    first commit

---
+++ analyze_single_folder_toy.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+from birdnet import SpeciesPredictions, predict_species_within_audio_file
+import time
+
+import pandas as pd
+
+folder_path = Path("/Users/aidanfauth/Library/CloudStorage/OneDrive-St.LawrenceUniversity/SLU Senior/Stat SYE/A001_SD001_toy")
+audio_files = list(folder_path.glob("*.WAV"))
+audio_files
+
+all_dfs = [] ## storage
+
+for audio_path in audio_files:
+    print(f"Processing {audio_path.name}...")
+
+    start = time.perf_counter()
+
+    # Run the prediction
+    predictions = SpeciesPredictions(
+        predict_species_within_audio_file(
+            audio_path,
+            min_confidence=0.4,
+            chunk_overlap_s=0.0
+        )
+    )
+
+    # Convert predictions to a DataFrame
+    df = pd.DataFrame([
+        {'start': start_t, 'end': end_t, **species_probs}
+        for (start_t, end_t), species_probs in predictions.items()
+    ])
+
+    # Add audio file identifier
+    df['file'] = audio_path.name 
+
+    end = time.perf_counter()
+    print(f"Finished {audio_path.name} in {end - start:.2f} seconds")
+
+    all_dfs.append(df)
+
+all_dfs
+combined_df = pd.concat(all_dfs, ignore_index=True)
+combined_df.to_csv("output.csv", index=False)
---
+++ process_single_folder.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+
+birdnet_results <- read_csv("output.csv", guess_max = 10000)
+problems(birdnet_results) |> print(n = Inf)
+
+View(birdnet_results)
+
+
+library(lubridate)
+library(hms)
+
+birdnet_long <- birdnet_results |> relocate(file) |>
+  pivot_longer(4:ncol(birdnet_results), names_to = "species", values_to = "probability") |>
+  mutate(date = str_sub(file, start = 1, end = 8),
+          hour = str_sub(file, 10, 11),
+          minute = str_sub(file, 12, 13),
+          second = str_sub(file, 14, 15)) |>
+  unite("date_time", c("date", "hour", "minute", "second"), sep = ":") |>
+  mutate(date_time = ymd_hms(date_time),
+          time = as_hms(date_time))
+
+birdnet_sightings <- birdnet_long |> filter(!is.na(probability))
+
+birdnet_most_prob <- birdnet_sightings |> group_by(file, start, end) |>
+  ## filter(probability == max(probability)) |>
+  filter(probability >= 0.75) |> 
+  ungroup() |> ## minimum probability to show in plot
+  mutate(species = fct_infreq(species)) |>
+  mutate(species = fct_rev(species))
+
+birdnet_most_prob
+
+ggplot(data = birdnet_most_prob, aes(x = time, y = species)) +
+  geom_jitter(aes(colour = probability), alpha = 0.5, height = 0.25) +
+  theme_minimal() +
+  scale_colour_viridis_c()

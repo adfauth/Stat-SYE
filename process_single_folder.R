0ae31be54ba9eca7ce98a8a8e6981ce53f25d0e5
Aidan Fauth <adfaut22@stlawu.edu>
2025-11-07 20:03:08 -0500

    updating species list

--- process_single_folder.R
+++ process_single_folder.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(here)
 
-birdnet_results <- read_csv(here("Output/output_A003_SD005_no_list.csv"), guess_max = 10000)
+birdnet_results <- read_csv(here("Output/output_A006_SD006_no_list.csv"), guess_max = 10000)
 problems(birdnet_results) |> print(n = Inf)
 
 species_list <- read_delim(here("Species_lists/species_list.txt"), delim = "\n", col_names = FALSE)

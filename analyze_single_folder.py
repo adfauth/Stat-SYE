e2167c26df765a9dae10325a6e3616f758b5576d
Aidan Fauth <adfaut22@stlawu.edu>
2025-12-03 23:19:14 -0500

    Ran ada and did some visualizations

--- analyze_single_folder.py
+++ analyze_single_folder.py
@@ -5,7 +5,7 @@ import time
 
 import pandas as pd
 
-folder_path = Path("A005_SD002")
+folder_path = Path("A004_SD012")
 audio_files = list(folder_path.glob("*.WAV"))
 audio_files
 
@@ -53,7 +53,7 @@ for audio_path in audio_files:
 
 all_dfs
 combined_df = pd.concat(all_dfs, ignore_index=True)
-combined_df.to_csv("output_A005_SD002_full_sp.csv", index=False)
+combined_df.to_csv("output_A004_SD012_full_sp.csv", index=False)
 
 
 
